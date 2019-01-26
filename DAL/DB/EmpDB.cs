2d5422161e613e3a3601073658396f1dbf7656fd
ashish <ashishkhare250@gmail.com>
2019-01-26 22:02:17 +0530

    Initial Commit

---
+++ DAL/DB/EmpDB.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DB
+{
+    public class EmpDB : DbContext
+    {
+        public EmpDB():base("name=Defalut")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ DAL/Migrations/201901251947180_first .cs
@@ -0,0 +1,42 @@
+namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class first : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Dob = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Addresses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Streat = c.String(),
+                        Employee_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Employees", t => t.Employee_Id)
+                .Index(t => t.Employee_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Addresses", "Employee_Id", "dbo.Employees");
+            DropIndex("dbo.Addresses", new[] { "Employee_Id" });
+            DropTable("dbo.Addresses");
+            DropTable("dbo.Employees");
+        }
+    }
+}
